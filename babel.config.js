3007c3e8406a105114130a4790f0bc7781ff40ce
Ajay Singh Yadav <ajayyadav442408@gmail.com>
2025-07-09 16:15:39 +0530

    delete update add

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
 };
-
-//JMOh1Ul7FqVBcLKS
-//mrajayyadav408
-//mongodb+srv://mrajayyadav408:JMOh1Ul7FqVBcLKS@cluster0.cr0wcrd.mongodb.net/
